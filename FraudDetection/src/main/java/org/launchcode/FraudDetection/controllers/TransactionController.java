d427233658182c49f5027303cf01864574c560ea
rahinasrahman <rshinasrahman94@gmail.com>
2025-09-08 21:33:34 -0500

    get all transaction controller

---
+++ FraudDetection/src/main/java/org/launchcode/FraudDetection/controllers/TransactionController.java
@@ -0,0 +1,23 @@
+package org.launchcode.FraudDetection.controllers;
+
+import org.launchcode.FraudDetection.models.Transaction;
+import org.launchcode.FraudDetection.models.data.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class TransactionController {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @GetMapping("/transactions")
+    public List<Transaction> getTransactions()
+    {
+        return (List<Transaction>) transactionRepository.findAll();
+    }
+}
